718554c1d651cf344442e91fa1787827f748a899
Jozsef Vesza <jozsef.vesza@outlook.com>
2015-05-11 22:41:05 +0200

    Added proper selection handling.

--- ExpandableTableView/TableViewController.swift
+++ ExpandableTableView/TableViewController.swift
@@ -14,23 +14,35 @@ let statusbarHeight: CGFloat = 20
 class TableViewController: UITableViewController {
     
     let viewModel = TableViewModel()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset.top = statusbarHeight
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ExpandableTableViewCell
         
         cell.mainTitle = viewModel.mainTitleForRow(indexPath.row)
         cell.detailTitle = viewModel.detailTitleForRow(indexPath.row)
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        
+        if let selectedIndex = tableView.indexPathForSelectedRow() where selectedIndex == indexPath {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            return nil
+        }
+        
+        return indexPath
     }
 }
